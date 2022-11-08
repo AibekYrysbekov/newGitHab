37449e016eff8448a90ad66a38c7f561aa201724
AibekYrysbekov <yrysbekovayba@gmail.com>
2022-11-08 21:05:56 +0600

    zadaniya

---
+++ 132.py
@@ -0,0 +1,37 @@
+'''
+lang1 = 'ruby'
+languages = ['go', 'java', 'php', 'python', 'javascript', 'ruby']
+
+for i in languages:
+    if i == lang1:
+        print(I)
+    else: pass
+'''
+'''
+
+languages = ['go', 'java', 'php', 'python', 'javascript', 'ruby']
+
+for i in languages:
+    print(i)
+    if i == 'php':
+        break
+'''
+'''
+a = 7
+for i in range(5):
+    a = a*a
+    print(a)
+'''
+
+
+'''
+languages = ['go', 'java', 'php', 'python', 'javascript', 'ruby']
+a = 0
+for i in languages:
+    print(str(a) + i)
+    a += 1
+
+print(list(range(10)))
+
+'''
+print(range(1, 11))
